d61e1ed77688f2406dfe96c4bccc4ddaa521e355
Eric Tang <tang8156@gmail.com>
2018-05-11 16:23:06 -0400

    Fixed: Handle Google Map failure

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Nav from './Nav.js'
 import { YELP_KEY, DEFAULT_GPS_LOCATION } from './Config.js'
 
 class App extends Component {
+  
+  errorFunc(app, error) {
+    this.setState({errors:`${app} => ${error}`})
+  }
+
   fetchBusinesses() {
     let options = {
       headers: {
@@ -19,12 +24,8 @@ class App extends Component {
           businesses: result.businesses
         })
       } else {
-        errorFunc('yelp', JSON.stringify(result.error))
+        this.errorFunc('yelp', JSON.stringify(result.error))
       }
-    }
-
-    var errorFunc = (app, error) => {
-      this.setState({errors:`${app} => ${error}`})
     }
 
     var url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=${DEFAULT_GPS_LOCATION.lat}&longitude=${DEFAULT_GPS_LOCATION.lng}&limit=8`
@@ -32,7 +33,7 @@ class App extends Component {
     fetch(url, options)
       .then(res => res.json())
       .then(data => processJson(data))
-      .catch(error => errorFunc('fetch', error))
+      .catch(error => {this.errorFunc('fetch', error)})
   }
 
   componentDidMount() {
@@ -60,11 +61,20 @@ class App extends Component {
     })
   }
 
+  mapFailed(app, error) {
+    this.errorFunc(app, error)
+  }
+
   render() {
     if(this.state == null) {
       return (<h2>Loading...</h2>)
     } else if(this.state.errors != null) {
-      return (<h2>{this.state.errors}</h2>)
+      return (
+        <div>
+          <h1>Something's wrong</h1>
+          <h4>{this.state.errors}</h4>
+        </div>
+        )
     } else {
       const { businesses, businessIDClicked } = this.state
       return (
@@ -74,6 +84,7 @@ class App extends Component {
             businessClicked={ this.businessClicked.bind(this) }>
           </Nav>
           <Map items={ businesses }
+            mapFailed={ this.mapFailed.bind(this) }
             businessIDClicked={ businessIDClicked }></Map>
         </div>);
     }
